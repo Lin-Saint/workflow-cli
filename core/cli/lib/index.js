10e05eb45e40866da936a6d613dc4f1c7110d5b4
saint <1134948440@qq.com>
2023-01-10 21:08:49 +0800

    降级文件权限

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,34 +3,39 @@ module.exports = core;
 const colors = require('colors/safe');
 const log = require('npmlog');
 const semver = require('semver');
-
-
+const rootCheck = require('root-check');
 const pkg = require('../../../package.json');
 
 const LOWVER_VERSION = '12.0.0';
 
 function core(params) {
-  try {
-    prepare();
-  } catch (error) {
-    log.error(log.error(colors.red(error.message)))
-  }
-  console.log('exec',params)
+	try {
+		prepare();
+	} catch (error) {
+		log.error(log.error(colors.red(error.message)));
+	}
+	console.log('exec', params);
 }
-
 
 function prepare() {
-  checkPkgVersion();
-  checkNodeVersion();
+	checkPkgVersion();
+	checkNodeVersion();
+	checkRoot();
 }
+
 function checkPkgVersion() {
-  console.log(pkg.version)
+	console.log(pkg.version);
 }
 
 function checkNodeVersion() {
-  const currentVersion = process.version;
-  console.log(currentVersion);
-  if (!semver.gte(currentVersion, LOWVER_VERSION)) {
-    throw new Error(`the node version need above ${ LOWVER_VERSION }`)
-  }
+	const currentVersion = process.version;
+	console.log(currentVersion);
+	if (!semver.gte(currentVersion, LOWVER_VERSION)) {
+		throw new Error(`the node version need above ${LOWVER_VERSION}`);
+	}
+}
+
+function checkRoot() {
+	rootCheck();
+	console.log(process.geteuid());
 }
